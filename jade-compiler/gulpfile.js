9d53cb8c347f84ba417ba2aee31dab3281ebea40
Tibs <tibs245@riseup.net>
2016-02-12 16:37:31 +0100

    fix: Replace files losts
    Replace files don't correct replaced

--- jade-compiler/gulpfile.js
+++ jade-compiler/gulpfile.js
@@ -1,16 +1,12 @@
 // Imports
 // -------------------------
 var gulp = require('gulp'),
-    jade      = require('gulp-jade'),
-    data        = require('gulp-data');
+    jade      = require('gulp-jade');
 
 // Generate html files from jade
 // -------------------------
 gulp.task('jade', function() {
     return gulp.src('src/jade/*.jade')
-        .pipe(data(function() {
-            return require('./src/data/settings.json');
-        }))
         .pipe(jade())
         .pipe(gulp.dest('dist/'));
 });
--- jade-dev/gulpfile.js
+++ jade-dev/gulpfile.js
@@ -2,15 +2,31 @@
 // -------------------------
 var gulp = require('gulp'),
     jade      = require('gulp-jade'),
+    del       = require('del'),
     data        = require('gulp-data');
 
 // Generate html files from jade
 // -------------------------
 gulp.task('jade', function() {
     return gulp.src('src/jade/*.jade')
-        .pipe(data(function() {
+        .pipe(data(function() {             // Insert json file
             return require('./src/data/settings.json');
         }))
-        .pipe(jade())
+        .pipe(jade({
+            pretty: true                    // False for compress code
+        }))
         .pipe(gulp.dest('dist/'));
+});
+
+// Watch task
+// -------------------------
+gulp.task('watch', function () {
+    gulp.watch('src/jade/**/*.jade', ['jade']);
+});
+
+
+// Clean task
+// -------------------------
+gulp.task('clean', function () {
+    return del('dist/*.html');
 });
